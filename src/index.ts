a69d3245d79671be45d0092530e6cb76fd56182c
UnEpicier <alexisvasseur96@gmail.com>
2024-01-15 16:13:16 +0100

    docs: create code structure example

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import morgan from 'morgan';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import router from './router/default';
+// import example from './example/example.routes';
 
 // ----------------------------------------------------- Server --------------------------------------------------------
 const app = express();
 
 app.use(morgan(process.env.NODE_ENV == 'development' ? 'dev' : 'combined'));
-app.use(router);
+
+// app.use(example);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
